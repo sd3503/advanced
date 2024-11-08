a777c1cc3cf0d849fb008a5b66ccb50a92de479a
SeonDong <sdkim@tssni.com>
2024-11-08 17:19:07 +0900

    Initial commit

---
+++ src/main/java/hello/advanced/app/V0/OrderServiceV0.java
@@ -0,0 +1,14 @@
+package hello.advanced.app.V0;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OrderServiceV0 {
+    private final OrderRepositoryV0 orderRepositoryV0;
+
+    public void orderItem(String itemId) {
+        orderRepositoryV0.save(itemId);
+    }
+}
---
+++ src/main/java/hello/advanced/trace/TraceId.java
@@ -0,0 +1,45 @@
+package hello.advanced.trace;
+
+import java.util.UUID;
+
+public class TraceId {
+
+    private String id;
+    private int level;
+
+    public TraceId() {
+        this.id = createId();
+        this.level = 0;
+    }
+
+    private TraceId(String id, int level) {
+        this.id = id;
+        this.level = level;
+    }
+
+    private String createId() {
+        // UUID 32자리 문자열이 생성되는데, 여기서는 8자리만 사용
+        return UUID.randomUUID().toString().substring(0, 8);
+    }
+
+    public TraceId createNextId() {
+        return new TraceId(id, level + 1);
+    }
+
+    public TraceId createPreviousId() {
+        return new TraceId(id, level - 1);
+    }
+
+    public boolean firstLevel() {
+        return level == 0;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+}
